a1ef06268e6f9f467946a869b70336bb8bcfcaca
pedro <paraoz22@gmail.com>
2021-04-20 23:27:36 -0300

    a done

--- src/main/scala/geoService/GeoClientDemo2.scala
+++ src/main/scala/geoService/GeoClientDemo2.scala
@@ -1,7 +1,10 @@
 package geoService
 
 import demo.geo.{GeoGetCountryCityByIPReq, GeoReply, GeoServiceGrpc}
-import io.etcd.jetcd.options.GetOption
+import io.etcd.jetcd.Watch.Listener
+import io.etcd.jetcd.options.{GetOption, WatchOption}
+import io.etcd.jetcd.watch.WatchEvent.EventType
+import io.etcd.jetcd.watch.WatchResponse
 import io.etcd.jetcd.{ByteSequence, Client, KeyValue}
 import io.grpc.ManagedChannelBuilder
 
@@ -15,50 +18,50 @@ object GeoClientDemo2 extends App {
 
   implicit val ec: ExecutionContextExecutor = ExecutionContext.global
 
-  def createStub(ip: String, port: Int = 50000): GeoServiceGrpc.GeoServiceStub = {
-    val builder = ManagedChannelBuilder.forAddress(ip, port)
-    builder.usePlaintext()
-    val channel = builder.build()
-
-    GeoServiceGrpc.stub(channel)
-  }
-
-  def readFile(path: String): List[String] = Source.fromFile(path).getLines.toList
-
-  def recursiveHell(ips: List[String]): Unit = {
-    val responses: List[Future[GeoReply]] = ips.map {
-      e =>
-        healthyStubs.head.getCountryCityByIP(GeoGetCountryCityByIPReq(e))
-    }
-    val futureGeoReplyList: Future[List[GeoReply]] = Future.sequence(responses)
-    futureGeoReplyList.onComplete { r =>
-      if (r.isFailure) {
-        println(healthyStubs.head + " is dead, removing from list")
-        healthyStubs = healthyStubs.tail
-        if (healthyStubs.isEmpty)
-          throw new RuntimeException("No more healthy stubs")
-        recursiveHell(ips)
-      } else {
-        println("Ding ding ding")
-        parse(r)
-      }
-    }
-  }
-
-  def parse(r: Try[List[GeoReply]]): Unit = {
-    val tupleList = r.get.map { e =>
-      val data = ujson.read(e.message)
-      (data("country").toString(), data("region").toString())
-    }
-    val finalList = tupleList.groupBy(_._1).map(e => (e._1, e._2.map(_._2)))
-    print(finalList)
-  }
-
-  def handleRequest(path: String): Unit = {
-    healthyStubs = Random.shuffle(healthyStubs)
-    val ips: List[String] = readFile(path)
-    recursiveHell(ips)
-  }
+//  def createStub(ip: String, port: Int = 50000): GeoServiceGrpc.GeoServiceStub = {
+//    val builder = ManagedChannelBuilder.forAddress(ip, port)
+//    builder.usePlaintext()
+//    val channel = builder.build()
+//
+//    GeoServiceGrpc.stub(channel)
+//  }
+//
+//  def readFile(path: String): List[String] = Source.fromFile(path).getLines.toList
+//
+//  def recursiveHell(ips: List[String]): Unit = {
+//    val responses: List[Future[GeoReply]] = ips.map {
+//      e =>
+//        healthyStubs.head.getCountryCityByIP(GeoGetCountryCityByIPReq(e))
+//    }
+//    val futureGeoReplyList: Future[List[GeoReply]] = Future.sequence(responses)
+//    futureGeoReplyList.onComplete { r =>
+//      if (r.isFailure) {
+//        println(healthyStubs.head + " is dead, removing from list")
+//        healthyStubs = healthyStubs.tail
+//        if (healthyStubs.isEmpty)
+//          throw new RuntimeException("No more healthy stubs")
+//        recursiveHell(ips)
+//      } else {
+//        println("Ding ding ding")
+//        parse(r)
+//      }
+//    }
+//  }
+//
+//  def parse(r: Try[List[GeoReply]]): Unit = {
+//    val tupleList = r.get.map { e =>
+//      val data = ujson.read(e.message)
+//      (data("country").toString(), data("region").toString())
+//    }
+//    val finalList = tupleList.groupBy(_._1).map(e => (e._1, e._2.map(_._2)))
+//    print(finalList)
+//  }
+//
+//  def handleRequest(path: String): Unit = {
+//    healthyStubs = Random.shuffle(healthyStubs)
+//    val ips: List[String] = readFile(path)
+//    recursiveHell(ips)
+//  }
 
   def bytes(str: String): ByteSequence = {
     ByteSequence.from(str.getBytes())
@@ -69,29 +72,41 @@ object GeoClientDemo2 extends App {
     val kVClient = client.getKVClient
     val prefix = bytes("/services/geo/")
     val option = GetOption.newBuilder().withPrefix(prefix).build()
-    val getFuture = kVClient.get(prefix, option) //todo esto deberia ser un prefix
+    val getFuture = kVClient.get(prefix, option)
     val response = getFuture.get
     val watchClient = client.getWatchClient
-//    watchClient.watch(bytes("/services/geo/"))
+    val watchOption = WatchOption.newBuilder().withPrefix(bytes("/services/geo/")).build()
+    watchClient.watch(bytes("/services/geo/"), watchOption, new Listener {
+      override def onNext(watchResponse: WatchResponse): Unit = {
+        val a = JavaConverters.asScalaBuffer(watchResponse.getEvents).toList
+        a.filter(_.getEventType.eq(EventType.DELETE)).foreach {
+          e =>
+            val ip = e.getKeyValue.getValue.getBytes.map(_.toChar).mkString
+            ips = ips.filterNot(_.eq(ip))
+        }
+      }
+
+      override def onError(throwable: Throwable): Unit = println("ERROR")
+
+      override def onCompleted(): Unit = println("COMPLETED!")
+    })
 //    etcdctl watch -- prefix service / geo //todo pasar a scala
-    val almost = JavaConverters.asScalaBuffer(response.getKvs).toList
-    almost.map(_.getValue.toString())
+    JavaConverters.asScalaBuffer(response.getKvs).toList.map(_.getValue.getBytes.map(_.toChar).mkString)
   }
 
-  //  asumimos que va a ser algo asi
-//  def notifyDeathOfService(key: String): Unit = {
-//    ips.filterNot(k => k.equals(client.get(key)))
-//  }
+//  val ips: List[String] = getServiceIps()
+//  val circularIps = Iterator.continually(ips).flatten
+//  val stub1 = createStub(circularIps.next(), 50004) //falta testear lo de los puertos en vivo
+//  val stub2 = createStub(circularIps.next(), 50003)
+//  val stub3 = createStub(circularIps.next(), 50002)
+//  val stub4 = createStub(circularIps.next(), 50000)
+//
+//  val stubs = List(stub1, stub2, stub3, stub4)
+//  var healthyStubs = stubs
+//  handleRequest("src/main/scala/geoService/ips.txt")
 
-  val ips: List[String] = getServiceIps()
-  val circularIps = Iterator.continually(ips).flatten
-  val stub1 = createStub(circularIps.next(), 50004) //falta testear lo de los puertos en vivo
-  val stub2 = createStub(circularIps.next(), 50003)
-  val stub3 = createStub(circularIps.next(), 50002)
-  val stub4 = createStub(circularIps.next(), 50000)
+  print("GEOCLIENT:")
+  var ips: List[String] = getServiceIps()
 
-  val stubs = List(stub1, stub2, stub3, stub4)
-  var healthyStubs = stubs
-  handleRequest("src/main/scala/geoService/ips.txt")
   System.in.read()
 }
--- src/main/scala/geoService/GeoService.scala
+++ src/main/scala/geoService/GeoService.scala
@@ -1,7 +1,13 @@
 package geoService
 
 import demo.geo._
+import io.etcd.jetcd.lease.LeaseKeepAliveResponse
+import io.etcd.jetcd.options.PutOption
+import io.etcd.jetcd.{ByteSequence, Client}
 import io.grpc.ServerBuilder
+import io.grpc.stub.StreamObserver
+
+import java.util.concurrent.CompletableFuture
 //import jetcd.EtcdClientFactory
 import scalaj.http.Http
 
@@ -71,14 +77,27 @@ class GeoService extends GeoServiceGrpc.GeoService {
 }
 
 object GeoServer extends App {
+  def bytes(str: String): ByteSequence = {
+    ByteSequence.from(str.getBytes())
+  }
 
-  def registerInETCD(etcdIp: String = "http://127.0.0.1:2379"): Unit = {
+  def registerInETCD(etcdIp: String = "http://etcd:2379"): Unit = {
     val localhost: InetAddress = InetAddress.getLocalHost
     val localIpAddress: String = localhost.getHostAddress
     println(localIpAddress)
+    val client = Client.builder.endpoints(etcdIp).build
+    val kVClient = client.getKVClient
+    val leaseClient = client.getLeaseClient
+    val id = "/services/geo/" + localIpAddress
 
-//    val client = EtcdClientFactory.newInstance(etcdIp)
-//    client.set("/services/geo/" + localIpAddress, localIpAddress, 10)
+    val leaseId: Long = leaseClient.grant(10).get().getID
+//    leaseClient.keepAlive(leaseId, new StreamObserver[LeaseKeepAliveResponse] {
+//      override def onNext(value: LeaseKeepAliveResponse): Unit = println("LEASE: " + value)
+//      override def onError(t: Throwable): Unit = {}
+//      override def onCompleted(): Unit = {}
+//    })
+    val option = PutOption.newBuilder().withLeaseId(leaseId).build()
+    kVClient.put(bytes(id), bytes(localIpAddress), option)
     //  se supone que la renovacion es automatica, pero como no podemos testear anda a saber.
   }
 
@@ -92,8 +111,7 @@ object GeoServer extends App {
   server.start()
 
   //  val ip: String = scala.io.StdIn.readLine(">")
-  println("Running....")
-
+  println("Running.... GEO SERVICE")
   registerInETCD()
   server.awaitTermination()
 }
